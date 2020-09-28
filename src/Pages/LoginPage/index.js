e102c473fe7e40ab2e7f55814f6bebe47fdf5d6f
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-09-27 22:12:14 -0700

    Added Material UI Password Sub component

--- src/Pages/LoginPage/index.js
+++ src/Pages/LoginPage/index.js
@@ -2,6 +2,15 @@ import React from "react";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
 
+// Form Components
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from "@material-ui/core/IconButton";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+
 const userStyles = makeStyles({
   container: {
     //Styling the Container
@@ -12,10 +21,11 @@ const userStyles = makeStyles({
 
     // Center align the content within the Container
     display: "flex",
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
 
-    // Vertical and Horizontally aligning the Container
+    // Vertically and Horizontally aligning the Container
     // Had to add height 100% to index.html to act as parent reference height
     position: "absolute",
     top: "25%",
@@ -25,9 +35,54 @@ const userStyles = makeStyles({
 
 const Login = () => {
   const classes = userStyles();
+  const [values, setValues] = React.useState({
+    password: "",
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   return (
     <div>
-      <Container className={classes.container}>Container</Container>
+      <Container className={classes.container}>
+        <div>Container</div>
+        <FormControl
+          className={(classes.margin, classes.textField)}
+          variant="outlined"
+        >
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={values.showPassword ? "text" : "password"}
+            value={values.password}
+            onChange={handleChange("password")}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={70}
+          />
+        </FormControl>
+      </Container>
     </div>
   );
 };
